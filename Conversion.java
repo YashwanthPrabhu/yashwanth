d286e47c76a5458401b68db6ebd8491a5bc4762f
Yashwanth Prabhu <YashwanthPrabhu@users.noreply.github.com>
2017-06-07 14:19:46 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Conversion {
+	public static void main(String args[])
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter an string");
+		String str=in.next();
+		int i=Integer.parseInt(str);
+		System.out.println("String to intger conversion "+i);
+	}
+
+}
