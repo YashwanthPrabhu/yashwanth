71f2e57e48742a2c84755f669a33c76e148c06a3
Yashwanth Prabhu <YashwanthPrabhu@users.noreply.github.com>
2017-06-08 12:30:55 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,30 +1,10 @@
-import java.util.Scanner;
-
-public class Palindrome {
-	public static void main(String[] args) {
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter a String");
-		String str=input.next();
-		String s1,s2;
-		int len=str.length();
-		for(int i=0;i<len;i++)
-		{
-			for(int j=i+1;j<len;j++)
-			{
-				if(str.charAt(i)==str.charAt(j))
-				{
-					s1=str.substring(i,j+1);
-					StringBuffer sb=new StringBuffer(s1);
-					StringBuffer ss=sb.reverse();
-					s2=new String(ss);
-					if(s1.equals(s2))
-					{
-						System.out.println(s1);
-					}
-					
-				}
-			}
-		}
-	}
-
+class Palindrome
+{
+public static void main(String args[])
+{
+StringBuffer s1= new StringBuffer(args[0]);
+StringBuffer s2=s1.reverse();
+String s3=s2.toString();
+System.out.println((args[0].equals(s3))?"Palindrome":"Not a Palindrome");
+}
 }
