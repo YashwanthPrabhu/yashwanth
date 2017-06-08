99eb7c21254ab2b832e73f42b1945717fcb8e5c0
Yashwanth Prabhu <YashwanthPrabhu@users.noreply.github.com>
2017-06-08 12:09:02 +0530

    Add files via upload

---
+++ Summ.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Summ {
+	public static void main(String args[])
+	{
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter two number");
+		int a=input.nextInt();
+		int b=input.nextInt();
+		int c=a+b;
+		System.out.println("Sum="+c);
+		
+	}
+
+}
