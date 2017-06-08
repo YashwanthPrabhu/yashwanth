0ebc883ea8dad9ef0970a7cb03df33be604eda6d
Yashwanth Prabhu <YashwanthPrabhu@users.noreply.github.com>
2017-06-08 14:10:11 +0530

    Add files via upload

---
+++ Palindrome1.java
@@ -0,0 +1,10 @@
+class Palindrome1
+{
+public static void main(String args[])
+{
+StringBuffer s1= new StringBuffer(args[0]);
+StringBuffer s2=s1.reverse();
+String s3=s2.toString();
+System.out.println((args[0].equals(s3))?"Palindrome":"Not a Palindrome");
+}
+}
