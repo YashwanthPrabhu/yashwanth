1b5b19bd254dbd468af2f415ecfee13a2a4e5ea8
Yashwanth Prabhu <YashwanthPrabhu@users.noreply.github.com>
2017-06-13 15:32:27 +0530

    Add files via upload

---
+++ NonRepeated.java
@@ -0,0 +1,36 @@
+public class NonRepeated { 
+	public static void main(String[] args)
+	{
+		int len=args.length;
+		if(len==0)
+		{
+			System.out.println("Please enter the values as command line arguements");
+		}
+		else
+		{
+			int count=0;
+			int arr[]=new int[len];
+			for(int i=0;i<len;i++)
+			{
+				arr[i]=Integer.parseInt(args[i]);
+			}
+					for(int i=0;i<len;i++)
+			{
+				for(int j=0;j<len;j++)
+				{
+					if(arr[i]==arr[j])
+					{
+						count++;
+					}
+				}
+				if(count==1)
+				{
+					System.out.println(arr[i]);
+					break;
+				}
+				count=0;
+			}
+		}
+	}
+
+}
